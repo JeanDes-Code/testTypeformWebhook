5d4d2dde2f06076c4a60a4120a6e1798d605ecc2
Jean Desauw <desauwjean@gmail.com>
2024-01-16 15:58:07 +0100

    testing hidden fields

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(express.json());
 
 app.post("/webhook", (req, res) => {
   console.log("Webhook Data:", req.body);
+  console.log("response fields email:", req.body.response_fields.email);
   res.status(200).send("Data received");
 });
 
